4fc20feb07e0106b8dd37b6e97c0798c5a5311ab
Branden Childers <branden.c.childers@gmail.com>
2022-01-23 20:05:02 -0600

    added 3 questions

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+var quizQuestions = [
+    {
+        question: "What is a JavaScript value type that has either a TRUE or FALSE value?",
+        correctAnswer: "c",
+        answers: {
+            a: "Number",
+            b: "String",
+            c: "Boolean",
+            d: "BigInt"
+        }
+    },
+    {
+        question: "Which of the following is considered a string?",
+        correctAnswer: "a",
+        answers: {
+            a: '"string"',
+            b: "string",
+            c: "string()",
+            d: "!string"
+        }
+    },
+    {
+        question: "Which Math Property would return the number 5?",
+        correctAnswer: "b",
+        answers: {
+            a: "Math.trunc(4.56)",
+            b: "Math.floor(5.64)",
+            c: "Math.round(6.54)",
+            d: "Math.ceil(5.65)"
+        }
+    },
+    {
+        question: "",
+        correctAnswer: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+            d: ""
+        }
+    }
+]
+
+// var askQuizQuestion = function() {
+//     var 
+
+//     for(var i = 0; i < quizQuestions.length; i++) {
+
+//     }
+// }
+
+// askQuestions()
